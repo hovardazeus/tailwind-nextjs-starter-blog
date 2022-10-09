e108afb75bf2408096f6b28ae75eace2f8244647
hovardazeus <hovardazeus@gmail.com>
2022-10-09 21:08:14 +0300

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,10 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Doğal Taşlar',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
